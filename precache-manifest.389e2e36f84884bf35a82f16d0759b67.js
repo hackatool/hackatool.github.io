e134304aa5df8668f7e5e5fcee34ee17aab8c9ba
Lucas Teixeira <ltx16@hotmail.com>
2019-09-22 16:15:24 -0300

    1st commit
    upload for demo purposes

---
+++ precache-manifest.389e2e36f84884bf35a82f16d0759b67.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1946530d7ebadfcadfa2dabba58a4a3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a1beae0a067a4031d37",
+    "url": "/static/css/main.3ae8c8bc.chunk.css"
+  },
+  {
+    "revision": "cda1dfde9197fd479565",
+    "url": "/static/js/2.8931b1da.chunk.js"
+  },
+  {
+    "revision": "6a1beae0a067a4031d37",
+    "url": "/static/js/main.42554fc9.chunk.js"
+  },
+  {
+    "revision": "82eab504bb604fc88077",
+    "url": "/static/js/runtime-main.e56d7ab7.js"
+  },
+  {
+    "revision": "df82dabdaebb965ff9b4c262ecd4bcd9",
+    "url": "/static/media/Arrow.df82dabd.svg"
+  },
+  {
+    "revision": "80af9c9b15153f814607e96fcb23bb9d",
+    "url": "/static/media/homealone.80af9c9b.png"
+  },
+  {
+    "revision": "76311791dcc88a595e02f1d3e8456542",
+    "url": "/static/media/logo.76311791.svg"
+  },
+  {
+    "revision": "90a0f3ae1057d770bfe1f901d7165a42",
+    "url": "/static/media/menu.90a0f3ae.svg"
+  }
+]);
